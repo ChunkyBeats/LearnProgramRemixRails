31eb2857ca792067123054ddcb32d51277291d2c
Cory Ribson & Steve Schneider <student@epicodus.com>
2015-06-30 16:29:34 -0700

    add fucntionality for sorting and dynamically updating lesson orders.

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,59 @@
+class LessonsController < ApplicationController
+  def index
+    @lessons = Lesson.all.sort_by {|obj| obj.number}
+  end
+
+  def new
+    @lesson = Lesson.new
+  end
+
+  def create
+    @lesson = Lesson.new(lesson_params)
+    @lesson.number = Lesson.all.length + 1
+    if @lesson.save
+      flash[:notice] = "LESSON SAVED, BRUH"
+      redirect_to lessons_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def destroy
+    @lesson = Lesson.find(params[:id])
+    @lesson.destroy
+    redirect_to lessons_path
+  end
+
+  def update
+    @lesson = Lesson.find(params[:id])
+    if @lesson.update(lesson_params)
+      sortedLessons = Lesson.all.sort_by {|obj| obj.number}
+      sortedLessons.slice!(sortedLessons.index(@lesson))
+      sortedLessons.insert( @lesson.number-1, @lesson)
+      sortedLessons.each do |lesson|
+        lesson.update(number: sortedLessons.index(lesson)+1)
+      end
+      redirect_to lessons_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @lesson = Lesson.find(params[:id])
+  end
+
+  # Lesson.all.forEach(lesson)
+  #   lesson.number = Lesson.all.index(lesson)
+
+
+  private
+  def lesson_params
+    params.require(:lesson).permit(:name, :language, :content, :number)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  resources :lessons
+
+end
